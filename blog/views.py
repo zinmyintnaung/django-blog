6e449c53e91abbb1a67b550a043b530b19724338
zinmyintnaung <zinmyintnaung@gmail.com>
2018-05-15 16:02:52 +0800

    bootstrap added

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def home(request, username):
+    posts = Post.objects.all()
+
     context = {
+        'posts':posts,
         'currentuser':username
     }
     return render(request, 'pages/home.html', context)
+
+def single(request, single):
+    post = Post.objects.get(id=single)
+    context = {
+        'post': post
+    }
+    return render(request, 'pages/single.html', context)
--- myblog/urls.py
+++ myblog/urls.py
@@ -4,6 +4,7 @@ from blog import views as blog_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^username/(?P<username>[-\w]+)/$', blog_views.home, name='username')
+    url(r'^user/(?P<username>[-\w]+)/$', blog_views.home, name='username'),
+    url(r'^post/(?P<single>[-\w]+)/$', blog_views.single, name='single')
 ]
 
