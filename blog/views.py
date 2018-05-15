b0e89f4f38a3672e933243e9f277c1f9d293bf84
zinmyintnaung <zinmyintnaung@gmail.com>
2018-05-15 16:37:45 +0800

    pagination added for post

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from blog.models import Post
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 # Create your views here.
-def home(request, username):
-    posts = Post.objects.all()
-
-    context = {
-        'posts':posts,
-        'currentuser':username
-    }
-    return render(request, 'pages/home.html', context)
+def home(request):
+    postList = Post.objects.all()
+    paginator = Paginator(postList, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'pages/home.html', {'posts':posts})
 
 def single(request, single):
     post = Post.objects.get(id=single)
--- myblog/urls.py
+++ myblog/urls.py
@@ -4,7 +4,7 @@ from blog import views as blog_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^user/(?P<username>[-\w]+)/$', blog_views.home, name='username'),
+    url(r'^', blog_views.home),
     url(r'^post/(?P<single>[-\w]+)/$', blog_views.single, name='single')
 ]
 
